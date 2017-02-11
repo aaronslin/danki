67dc96777de23f535ee6fc7396ec94fd0e007e4f
Aaron Lin <lin.aaron1@gmail.com>
2017-02-11 17:47:44 -0500

    added basic functionality for adding a new card to the deck

--- danki.js
+++ danki.js
@@ -3,32 +3,76 @@
 // There are probably some better data structures besides 
 // whatever the heck we're using
 
-// When creating a new deck, check that "decks" isn't allowed
-
+var DECK_NAMES_KEY = "ALLDECKS";
+var EMPTY_LIST = [];
+var BANNED_DECK_NAMES = [DECK_NAMES_KEY, ""];
+var FRONT_KEY = "FRONT";
+var BACK_KEY = "BACK";
+var SRS_KEY = "SRS";
+var SRS_DEFAULT = 1;
 
 $(document).ready(function() {
 	onloadProcedures();
 
 	$("#addDeckForm").submit(function(event) {
 		event.preventDefault();
-		var bannedDeckNames = ["decks"];
-		var deckName = $("#addDeckInput").val();
-		if(listContainsEl(bannedDeckNames, deckName)) {
-			console.log("Deck name not permitted:", deckName);
-			return;
-		}
-		chrome.storage.local.set({[deckName]: []}, function(result) {
-			alertUser("Deck successfully created:", deckName);
-		});
+		addNewDeck();
 	});
+
+	$("#addCardForm").submit(function(event) {
+		event.preventDefault();
+		addNewCard();
+	})
 });
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-	updateJSONDisplay();
+	onloadProcedures();
 });
 
 function onloadProcedures() {
 	updateJSONDisplay();
+	updateDeckSelection();
+}
+
+function updateDeckSelection() {
+	$("#addCardSelect option").remove();
+	chrome.storage.local.get(DECK_NAMES_KEY, function(deckNamesObj) {
+		deckNames = deckNamesObj[DECK_NAMES_KEY];
+		console.log(deckNamesObj)
+		$.each(deckNames, function(key, value) {
+			$("#addCardSelect").append(new Option(value, value));
+		});
+	});
+}
+
+function addNewDeck() {
+	var deckName = $("#addDeckInput").val();
+	if(listContainsEl(BANNED_DECK_NAMES, deckName)) {
+		console.log("Deck name not permitted:", deckName);
+		return;
+	}
+	pushToStorageList(DECK_NAMES_KEY, deckName, function(result) {
+		chrome.storage.local.set({[deckName]: []}, function(result) {
+			alertUser("Deck successfully created:", deckName);
+		});
+	});
+	$("#addDeckInput").val("");
+}
+
+function addNewCard() {
+	var front = String($("#addCardFront").val());
+	var back = String($("#addCardBack").val());
+	var deckName = $("#addCardSelect").val();
+
+	if(front.trim() == "" || back.trim() == "") {
+		return;
+	}
+	var cardToStore = {[FRONT_KEY]: front, 
+					[BACK_KEY]: back,
+					[SRS_KEY]: SRS_DEFAULT};
+	pushToStorageList(deckName, cardToStore, function(result) {
+		console.log("Card successfully added");
+	});
 }
 
 function updateJSONDisplay() {
@@ -38,10 +82,37 @@ function updateJSONDisplay() {
 	});
 }
 
-function storeCard(deckName, cardToStore) {
-	var ifNoDeckFound = [];
-	chrome.storage.local.get({"decks": ifNoDeckFound}, function(decks) {
-		alertUser("decks", decks);
+function pushToStorageList(key, element, callback) {
+	chrome.storage.local.get({[key]: EMPTY_LIST}, function(storageListObj) {
+		storageList = storageListObj[key];
+		if(listContainsEl(storageList, element)) {
+			return;
+		}
+		storageList.push(element);
+		chrome.storage.local.set({[key]: storageList}, function(result) {
+			callback(result);
+		});
+	});
+}
+
+function listContainsEl(list, element) {
+	return list.indexOf(element) != -1;
+}
+
+
+
+
+/* ------------ DEPRECATED ------------ */
+
+
+
+function alertUser(message1, message2) {
+	console.log(message1, message2);
+}
+
+function DEPRECATED_storeCard(deckName, cardToStore) {
+	chrome.storage.local.get({DECK_NAMES_KEY: EMPTY_LIST}, function(decksObj) {
+		decks = decksObj[DECK_NAMES_KEY];
 
 		/* Check if deckName exists as a key */
 		validDeckNames = decks.filter(function(elem) {
@@ -52,20 +123,9 @@ function storeCard(deckName, cardToStore) {
 			return;
 		}
 
-		chrome.storage.local.get({[deckName]: []}, function(deck) {
-			deck.push(cardToStore);
-			chrome.storage.local.set({[deckName]: deck}, function(result) {
-				alertUser("Deck successfully updated:", deck);
-			});
+		pushToStorageList(deckName, cardToStore, function(result) {
+			console.log("Card successfully added");
 		});
 	});
-}
-
-function listContainsEl(list, element) {
-	return list.indexOf(element) != -1;
-}
-
-function alertUser(message1, message2) {
-	console.log(message1, message2);
 }
 
