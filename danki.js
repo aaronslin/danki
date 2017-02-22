2a689f0c3b51c5c9a51dbba108db0cf8a0422a78
Aaron Lin <lin.aaron1@gmail.com>
2017-02-22 16:29:31 -0500

    added basic functionality for when a card is reviewed; update storage, change globals, update HTML

--- danki.js
+++ danki.js
@@ -8,25 +8,48 @@ var ACTIVE_DECK_KEY = "ACTIVE_DECKS";
 var INACTIVE_DECK_KEY = "INACTIVE_DECKS";
 var EMPTY_LIST = [];
 var BANNED_DECK_NAMES = [ACTIVE_DECK_KEY, INACTIVE_DECK_KEY, ""];
+	// You should really concat this with localStoreObj[(IN)ACTIVE_DECK_KEY]
 
 // Card storage key names 
 var FRONT_KEY = "FRONT";
 var BACK_KEY = "BACK";
-var BUCKET_KEY = "SRS";
+var BUCKET_KEY = "BUCKET";
 var BUCKET_DEFAULT = 1;
 var CREATED_KEY = "ADDED";
 var NEXT_REVIEW_KEY = "NEXT_REVIEW";
+var DECK_NAME_KEY = "DECK_NAME";
 
 // Other global variables
 var SRS_ALGORITHM = srs_Leitner;
 var FREQUENCY = 4000;
-var NEXT_REVIEW_TIME = 2; // null; // Use null to denote that a card is currently displayed
-var EXPIRED_CARDS_QUEUE = [];
+var NEXT_REVIEW_TIME = 2; // null;
+	// Use null to denote that a flashcard is displayed
+var CURRENT_CARD = null;
+var VALID_FEEDBACK = "145";
+var AWAITING_FEEDBACK = false;
 
 
 $(document).ready(function() {
 	onloadProcedures();
 
+	// Top panel functions
+	$("#flashcardFront").click(function(event) {
+		displayFlashcardBack();
+	});
+
+	$(document).keypress(function(press) {
+		pressedKey = String.fromCharCode(press.which);
+		if (!AWAITING_FEEDBACK) {
+			return;
+		}
+		if (VALID_FEEDBACK.indexOf(pressedKey) != -1) {
+			// check that this doesn't conflict with textboxes
+			finishFlashcard(pressedKey);
+		}
+	});
+
+
+	// Bottom panel functions
 	$("#addDeckForm").submit(function(event) {
 		event.preventDefault();
 		addNewDeck();
@@ -35,10 +58,6 @@ $(document).ready(function() {
 	$("#addCardForm").submit(function(event) {
 		event.preventDefault();
 		addNewCard();
-	});
-
-	$("#flashcardFront").click(function(event) {
-		displayFlashcardBack();
 	});
 });
 
@@ -53,7 +72,6 @@ var checkExpirationInterval = setInterval(function() {
 				return;
 			}
 			displayFlashcardFront(nextCard);
-			NEXT_REVIEW_TIME = null;
 		});
 	}
 		/* If next_review_time passed:
@@ -76,34 +94,106 @@ var checkExpirationInterval = setInterval(function() {
 			(2) NEXT_REVIEW_TIME has passed
 	*/
 
-}, 4000);
+}, FREQUENCY);
+
+
+
+
+
+// ------------------- TOP PANEL METHODS -------------------
 
 function displayFlashcardFront(nextCard) {
+	if (AWAITING_FEEDBACK) {
+		return;
+	}
 	$("#flashcardFront").removeClass("inactiveCard").addClass("activeCard");
 	$("#flashcardBack").removeClass("activeCard").addClass("inactiveCard");
+	$("#flashcardSeparator").addClass("disappear");	
 
-	$("#flashcardFront > .flashcardInner").html(nextCard[FRONT_KEY])
-	$("#flashcardBack > .flashcardInner").html(nextCard[BACK_KEY])
+	$("#flashcardFront > .flashcardInner").html(nextCard[FRONT_KEY]);
+	$("#flashcardBack > .flashcardInner").html(nextCard[BACK_KEY]);
+	
+	NEXT_REVIEW_TIME = null;
+	CURRENT_CARD = nextCard;
 }
 
 function displayFlashcardBack() {
 	if ($("#flashcardFront").hasClass("activeCard")) {
-	$("#flashcardBack").removeClass("inactiveCard").addClass("activeCard");
-		
+		$("#flashcardBack").removeClass("inactiveCard").addClass("activeCard");	
+		$("#flashcardSeparator").removeClass("disappear");	
 	}
+	AWAITING_FEEDBACK = true;
+}
+
+function finishFlashcard(feedback) {
+	var newCard = CURRENT_CARD;
+	var newBucket = SRS_ALGORITHM(CURRENT_CARD[BUCKET_KEY], feedback);
+	var nextReview = bucketToNextReview(newBucket)
+	newCard[BUCKET_KEY] = newBucket;
+	newCard[NEXT_REVIEW_KEY] = nextReview;
+
+	var deckName = newCard[DECK_NAME_KEY];
+
+	chrome.storage.local.get(deckName, function(deckObj) {
+		deck = deckObj[deckName];
+		console.log(deckObj, deckName)
+		deck = removeOneFromArraySuchThat(deck, function(card) {
+			return card[CREATED_KEY] == newCard[CREATED_KEY] && 
+					card[FRONT_KEY] == newCard[FRONT_KEY] &&
+					card[BACK_KEY] == newCard[BACK_KEY];
+		});
+		deck.push(newCard);
+		chrome.storage.local.set({[deckName]: deck}, function() {
+			console.log("Card successfully updated:", newCard[FRONT_KEY]);
+			return;
+		});
+	});
+
+	AWAITING_FEEDBACK = false;
+	CURRENT_CARD = null;
+
+	chrome.storage.local.get(null, function(localStoreObj) {
+		nextCard = getNextCardByTime(localStoreObj);
+		nextReviewTime = nextCard[NEXT_REVIEW_KEY];
+			// Notice that prevents you from seeing the same card twice!
+			// To revert, use Math.min(^, nextReview); Don't forget about async.
+		if (nextReviewTime < Date.now()) {
+			displayFlashcardFront(nextCard);
+		} else {
+			NEXT_REVIEW_TIME = nextReviewTime;
+		}
+	});
+
+
+}
+
+function removeOneFromArraySuchThat(array, criterion) {
+	console.log(array)
+	for(var i=0; i<array.length; i++) {
+		if (criterion(array[i])) {
+			array.splice(i,1);
+			return array;
+		}
+	}
+	return null;
 }
 
 function getNextCardByTime(localStoreObj) {
+	activeCards = getActiveCards(localStoreObj);
+
+	return argmin(activeCards, function(card) {
+		return card[NEXT_REVIEW_KEY]
+	});
+}
+
+function getActiveCards(localStoreObj) {
 	decks = localStoreObj[ACTIVE_DECK_KEY];
 	activeCards = [];
 	for(var i=0; i<decks.length; i++) {
 		deckName = decks[i];
 		activeCards = activeCards.concat(localStoreObj[deckName]);
 	}
-
-	return argmin(activeCards, function(card) {
-		return card[NEXT_REVIEW_KEY]
-	});
+	return activeCards;
 }
 
 function argmin(list, criterion) {
@@ -121,26 +211,6 @@ function argmin(list, criterion) {
 	return minArg;
 }
 
-function getExpiredCards(localStoreObj) {
-	// deprecated!
-	decks = localStoreObj[ACTIVE_DECK_KEY];
-	activeCards = [];
-	for(var i=0; i<decks.length; i++) {
-		deckName = decks[i];
-		activeCards = activeCards.concat(localStoreObj[deckName]);
-	}
-
-	expiredCards = activeCards.filter(function(cardObj) { 
-			var nextReview = cardObj[NEXT_REVIEW_KEY];
-			return typeof(nextReview) == "number" && nextReview < NEXT_REVIEW_TIME;
-		}).sort(function(card1, card2) {
-			// Cards are sorted backwards by expiration date
-			return card2[NEXT_REVIEW_KEY]-card1[NEXT_REVIEW_KEY];
-		});
-	return expiredCards;
-}
-
-
 chrome.storage.onChanged.addListener(function(changes, namespace) {
 	onloadProcedures();
 });
@@ -154,6 +224,36 @@ function onloadProcedures() {
 function updateNextCardInfo() {
 	// TODO: make sure that NEXT_REVIEW_TIME isn't null
 }
+
+
+/* https://en.wikipedia.org/wiki/Leitner_system 
+	Using a modified algorithm to not completely reset bucket upon wrong answer */
+function srs_Leitner(currBucketNum, feedback) {
+	var minBucket = BUCKET_DEFAULT;
+	var maxBucket = 5;
+
+	if (feedback==1) {
+		var newBucket = Math.floor(currBucketNum/2);
+		return Math.max(minBucket, newBucket);
+	}
+	if (feedback==4) {
+		return Math.min(maxBucket, currBucketNum+1);
+	}
+	else {
+		return currBucketNum;
+	}
+}
+
+function bucketToNextReview(bucketNum) {
+	noise = 1;
+	return temporaryNextReview();
+}
+
+
+
+
+
+// ------------- BOTTOM PANEL METHODS ----------------
 
 function updateDeckChoicesHTML() {
 	$("#addCardSelect option").remove();
@@ -191,7 +291,8 @@ function addNewCard() {
 						[BACK_KEY]: back,
 						[BUCKET_KEY]: BUCKET_DEFAULT,
 						[NEXT_REVIEW_KEY]: temporaryNextReview(),
-						[CREATED_KEY]: Date.now()
+						[CREATED_KEY]: Date.now(),
+						[DECK_NAME_KEY]: deckName
 	};
 
 	/* TODO: change the NEXT_REVIEW_KEY value */
@@ -225,29 +326,16 @@ function pushToStorageList(key, element, callback) {
 	});
 }
 
+
+
+
 function listContainsEl(list, element) {
 	return list.indexOf(element) != -1;
 }
 
 
-/* https://en.wikipedia.org/wiki/Leitner_system 
-	Using a modified algorithm to not completely reset bucket upon wrong answer */
-function srs_Leitner(bucketNum, isCorrect) {
-	var minBucket = BUCKET_DEFAULT;
-	var maxBucket = 5;
-	if (isCorrect) {
-		return Math.min(maxBucket, bucketNum+1);
-	}
-	else {
-		var newBucket = Math.floor(bucketNum/2);
-		return Math.max(minBucket, newBucket);
-	}
-}
 
-function srsBucketToTime(bucketNum) {
-	noise = 1;
 
-}
 
 /* ------------ DEPRECATED ------------ */
 
@@ -274,5 +362,25 @@ function DEPRECATED_storeCard(deckName, cardToStore) {
 			console.log("Card successfully added");
 		});
 	});
+}
+
+
+function DEPRECATED_getExpiredCards(localStoreObj) {
+	// deprecated!
+	decks = localStoreObj[ACTIVE_DECK_KEY];
+	activeCards = [];
+	for(var i=0; i<decks.length; i++) {
+		deckName = decks[i];
+		activeCards = activeCards.concat(localStoreObj[deckName]);
+	}
+
+	expiredCards = activeCards.filter(function(cardObj) { 
+			var nextReview = cardObj[NEXT_REVIEW_KEY];
+			return typeof(nextReview) == "number" && nextReview < NEXT_REVIEW_TIME;
+		}).sort(function(card1, card2) {
+			// Cards are sorted backwards by expiration date
+			return card2[NEXT_REVIEW_KEY]-card1[NEXT_REVIEW_KEY];
+		});
+	return expiredCards;
 }
 
